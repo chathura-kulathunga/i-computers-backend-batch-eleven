422633471b2993c9e2f4a2a1464f3bc8e22c68bb
Chathura Kulathunga <chathurakulathunga.foxc@gmail.com>
2026-03-30 21:07:16 +0530

    ..

--- models/product.js
+++ models/product.js
@@ -1,63 +1,52 @@
-import mongoose, { model } from 'mongoose';
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
     {
-        productId : {
-            type : String,
-            unique : true,
-            required : true
+        productId: {
+            type: String,
+            unique: true,
+            required: true
         },
         name : {
-            type : String,
-            required : true
+            type: String,
+            required: true
         },
         altNames : {
-            type : [String],
-            default : [],
-            required : true
+            type: [String],
+            default: [],
+            required: true
         },
-        description : {
-            type : String,
-            required : true
+        description :{
+            type: String,
+            required: true
         },
-        price : {
-            type : Number,
-            required : true
+        price :{
+            type: Number,
+            required: true
         },
-        labelledPrice : {
-            type : Number,
-            required : true
+        labeledPrice : {
+            type: Number,
+            required: true
         },
-        images : {
-            type : [String],
-            default : ["/default-product-1.png", "/default-product-2.png"],
-            required : true
+        images :{
+            type: [String],
+            default: ["/default-product-1.png", "/default-product-2.png"],
+            required: true
         },
         isAvailable : {
-            type : Boolean,
-            required : true,
-            default : true
+            type: Boolean,
+            required: true,
+            default: true,
         },
-        category : {
-            type : String,
-            required : false
+        category :{
+            type: String,
+            required: false
         },
-        stock : {
-            type : Number,
-            required : true,
-            default : 0
+        stock:{
+            type: Number,
+            required: true,
+            default: 0
         },
-        brand : {
-            type : String,
-            required : false
-        },
-        model : {
-            type : String,
-            required : false
-        }
+        
     }
 )
-
-const Product = mongoose.model("product" , productSchema)
-
-export default Product
